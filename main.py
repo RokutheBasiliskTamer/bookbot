40bf19fd6b62280f1c658a263a3844449a0ae72a
RokutheBasiliskTamer <bduckworth99@gmail.com>
2024-12-12 15:10:41 -0500

    created main.py with char and word count functions and added .gitignore

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    print(count_words(file_contents))
+    print(count_chars(file_contents))
+
+def count_words(text):
+    return len(text.split())
+
+def count_chars(text):
+    chars_seen = {}
+    text = text.lower()
+    for char in text:
+        if char in chars_seen:
+            chars_seen[char] += 1
+        else:
+            chars_seen[char] = 1
+    return chars_seen
+
+main()
