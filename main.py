cfd7b81703a09dde500908c9f6ec15f0ce163905
RokutheBasiliskTamer <bduckworth99@gmail.com>
2024-12-12 15:40:29 -0500

    added function to generate report and sort characters

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(count_words(file_contents))
-    print(count_chars(file_contents))
+    
+    run_report("books/frankenstein.txt")
 
 def count_words(text):
     return len(text.split())
 
 def count_chars(text):
-    chars_seen = {}
+    chars_seen = []
     text = text.lower()
+    dict_list = []
     for char in text:
         if char.isalpha():
             if char in chars_seen:
-                chars_seen[char] += 1
+                for dictionary in dict_list:
+                    if dictionary["char"] == char:
+                        dictionary["num"] += 1
             else:
-                chars_seen[char] = 1
-    return chars_seen
+                new_letter = {"char": char,
+                              "num": 1}
+                dict_list.append(new_letter)
+                chars_seen.append(char)
 
+    return dict_list
+
+def sort_on(dict):
+    return dict["num"]
+
+def run_report(filename):
+    with open(filename) as f:
+        file_contents = f.read()
+    word_count = count_words(file_contents)
+    letter_counts = count_chars(file_contents)
+    letter_counts.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {filename} ---")
+    print(f"{word_count} words found in the document")
+    for letter in letter_counts:
+        print(f"The '{letter["char"]}' character was found {letter["num"]} times")
+    print("--- End report ---")
 main()
